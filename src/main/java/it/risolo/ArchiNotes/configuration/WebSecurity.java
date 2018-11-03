597d47be7d9933a300bf3204395957537b819d54
Vincenzo <vinz.risolo@gmail.com>
2018-11-03 01:06:22 +0100

    angular

--- src/main/java/it/risolo/ArchiNotes/configuration/WebSecurity.java
+++ src/main/java/it/risolo/ArchiNotes/configuration/WebSecurity.java
@@ -36,6 +36,10 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
 		
 		 http.cors().and().csrf().disable().authorizeRequests()
          .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
+         .antMatchers("/").permitAll()
+         .antMatchers("/app/**").permitAll()
+         .antMatchers("/node_modules/**").permitAll()
+         .antMatchers("*.css").permitAll()
          .anyRequest().authenticated()
          .and()
 	         .addFilter(new JwtAuthenticationFilter(authenticationManager()))
